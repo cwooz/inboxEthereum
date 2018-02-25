ab3c014d614b976096b831c2be4009511308d496
cwooz <woosley.christopher@gmail.com>
2018-02-25 18:00:57 -0500

    comment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -4,27 +4,30 @@ const Web3    = require('web3');
 const web3    = new Web3(ganache.provider());
 
 
-class Car {
-  park() {
-    return 'stopped';
-  }
 
-  drive() {
-    return 'vroom';
-  }
-}
 
-let car;
 
-beforeEach(() => {
-  car = new Car();  
-});
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  });
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  });
-});
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//   car = new Car();  
+// });
+
+// describe('Car', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// });
