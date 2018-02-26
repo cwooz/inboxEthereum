8ab890e8cee85e015a32f0f241ac1e8095108134
cwooz <woosley.christopher@gmail.com>
2018-02-25 20:30:37 -0500

    deploy.js

--- deploy.js
+++ deploy.js
@@ -3,5 +3,8 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'smooth proud garment mean talk account waste because diesel desk define course'
+  'smooth proud garment mean talk account waste because diesel desk define course',
+  'https://rinkeby.infura.io/8XOYsoLLg4vk8ejb8qxa'
 );
+
+const web3 = new Web3(provider);
