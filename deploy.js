5431b91024001caddf1956d782e67dc825bcff06
cwooz <woosley.christopher@gmail.com>
2018-02-25 20:27:01 -0500

    deploy.js

---
+++ deploy.js
@@ -0,0 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'smooth proud garment mean talk account waste because diesel desk define course'
+);
